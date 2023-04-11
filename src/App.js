89f1ed00ef0aa3fab5ab16e6039a89acbe5b7ba6
CiprianBoamba <ciprian.boamba@gmail.com>
2023-04-11 15:56:55 +0100

    add images&card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+// Import useState from React to help manage state in your app
+import { useState } from 'react';
 import './App.css';
 
+// This is a list of card images
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
+
 function App() {
+  // Create a state for cards and a function to update it
+  const [cards, setCards] = useState([]);
+
+  // Create a state for turns and a function to update it
+  const [turns, setTurns] = useState(0);
+
+  // This function shuffles the cards and updates the state
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+
+      // Sort cards randomly
+      .sort(() => Math.random() - 0.5)
+      // Assign a random id to each card
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    // Update the state with the shuffled cards
+    setCards(shuffleCards);
+    // Reset the turns state to 0
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
